a7f2ed99c8ce16ba66ec55eb2d162bde56719a9b
Pete <ppete.st10@gmail.com>
2024-06-27 15:50:48 +0700

    updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
 import static ui.MainUI.start;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("JO");
         start();
     }
 
